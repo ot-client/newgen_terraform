b3d55c6e4f9cfb8cf52165b5afe37ffb774a81d5
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-03-23 12:44:49 +0530

    updated variable.tf and main.tf for adding sg rule

--- modules/terraform-aws-ec2-compute/variable.tf
+++ modules/terraform-aws-ec2-compute/variable.tf
@@ -41,10 +41,12 @@ variable "firewall_instance_key" {
 variable "security_group_ports" {
   description = "Ingress port configuration mapped to SG name patterns"
   type = map(object({
-    from_port  = number
-    to_port    = number
-    protocol   = string
-    name_regex = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    name_regex  = string
+    cidr_blocks = list(string)
+    type        = string
   }))
 }
 
