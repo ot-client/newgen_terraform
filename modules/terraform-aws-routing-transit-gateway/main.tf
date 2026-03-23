7410035a361a0124d5d36c0cbfa4a9dea65ce6aa
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-03-23 16:47:25 +0530

    updating module files for transit gateway routing

--- modules/terraform-aws-routing-transit-gateway/main.tf
+++ modules/terraform-aws-routing-transit-gateway/main.tf
@@ -1,8 +1,7 @@
 resource "aws_route" "tgw_routes" {
-  for_each = { for vpc in var.vpc_attachments : vpc.name => vpc if vpc.route_table_id != null }
+  for_each = toset(var.route_table_ids)
 
-  route_table_id         = each.value.route_table_id
+  route_table_id         = each.value
   destination_cidr_block = var.tgw_route_cidr_block
   transit_gateway_id     = data.aws_ec2_transit_gateway.existing_tgw.id
-
 }
--- modules/terraform-aws-routing-transit-gateway/output.tf
+++ modules/terraform-aws-routing-transit-gateway/output.tf
@@ -1,3 +1,4 @@
-output "vpc_attachment_ids" {
-  value = { for k, v in aws_ec2_transit_gateway_vpc_attachment.tgw_attachment : k => v.id }
+output "tgw_route_ids" {
+  description = "Map of route table ID to the created TGW route ID"
+  value       = { for k, v in aws_route.tgw_routes : k => v.id }
 }
