87c5c39ea75ac83f8673e8976bac93f1bf7a27c0
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-03-25 12:42:57 +0530

    removing the concept of routing from transit gateway attachment

--- modules/terraform-aws-transit-gateway/main.tf
+++ modules/terraform-aws-transit-gateway/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment" {
-  for_each = { for vpc in var.vpc_attachments : vpc.name => vpc }
+  for_each = { for vpc in var.attachment_name : vpc.name => vpc }
 
   subnet_ids         = each.value.subnet_ids
   transit_gateway_id = data.aws_ec2_transit_gateway.existing_tgw.id
