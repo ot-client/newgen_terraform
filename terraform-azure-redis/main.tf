099dcc07c991e4325598ba77c673706a7f3607d2
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-03-25 18:32:25 +0530

    adding redis module

---
+++ terraform-azure-redis/main.tf
@@ -0,0 +1,55 @@
+resource "azurerm_redis_cache" "redis" {
+  name                = var.redis_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  capacity            = var.capacity
+  family              = "C"
+  sku_name            = var.sku_name
+  
+  # Security settings as per client requirements
+  minimum_tls_version = "1.2"
+  
+  # Network settings - Private Endpoint connectivity
+  public_network_access_enabled = false
+  
+  tags = var.tags
+}
+
+# Private DNS Zone for Redis
+resource "azurerm_private_dns_zone" "redis_dns" {
+  name                = var.private_dns_zone_name
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+# Private DNS Zone Virtual Network Link
+resource "azurerm_private_dns_zone_virtual_network_link" "redis_dns_link" {
+  name                  = var.private_dns_zone_virtual_network_link_name
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.redis_dns.name
+  virtual_network_id    = var.virtual_network_id
+  registration_enabled  = false
+  tags                  = var.tags
+}
+
+# Private Endpoint for Redis
+resource "azurerm_private_endpoint" "redis_pe" {
+  name                = "${var.redis_name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "${var.redis_name}-psc"
+    private_connection_resource_id = azurerm_redis_cache.redis.id
+    subresource_names              = ["redisCache"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "redis-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.redis_dns.id]
+  }
+
+  tags = var.tags
+}
