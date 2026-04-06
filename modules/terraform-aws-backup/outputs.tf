2957ad77fc9410dc3eb447bdb9fe51f97f23e697
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-04-07 00:02:30 +0530

    Update outputs.tf

--- modules/terraform-aws-backup/outputs.tf
+++ modules/terraform-aws-backup/outputs.tf
@@ -1,9 +1,9 @@
 output "vault_arn" {
-  value = aws_backup_vault.this.arn
+  value = aws_backup_vault.vault.arn
 }
 
 output "plan_id" {
-  value = aws_backup_plan.this.id
+  value = aws_backup_plan.plan.id
 }
 
 output "iam_role_arn" {
