0ca60b7599113fc9dedfa4ab8eb5e7fc6417eef5
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-04-10 17:06:31 +0530

    updating files as per client requirement

--- modules/terraform-aws-launch-template/main.tf
+++ modules/terraform-aws-launch-template/main.tf
@@ -1,28 +1,37 @@
 resource "aws_launch_template" "template" {
-  region        = var.region
-  name          = var.name
-  instance_type = var.instance_type
-   metadata_options {
-    http_endpoint               = var.metadata_http_endpoint
-    http_tokens                 = var.metadata_http_tokens
-    http_put_response_hop_limit = var.metadata_hop_limit
-  }
+  for_each = var.launch_templates
 
+  name          = each.key
+  instance_type = each.value.instance_type
+
+  metadata_options {
+    http_endpoint               = each.value.metadata_http_endpoint
+    http_tokens                 = each.value.metadata_http_tokens
+    http_put_response_hop_limit = each.value.metadata_hop_limit
+  }
 
   block_device_mappings {
-    device_name = var.volume_device_name
+    device_name = each.value.volume_device_name
 
     ebs {
-      volume_size           = var.volume_size
-      volume_type           = var.volume_type
-      delete_on_termination = var.delete_on_termination
-      encrypted             = var.encrypted
-      throughput            = var.throughput
+      volume_size           = each.value.volume_size
+      volume_type           = each.value.volume_type
+      delete_on_termination = each.value.delete_on_termination
+      encrypted             = each.value.encrypted
+      throughput            = each.value.throughput
     }
   }
-  tag_specifications {
-  resource_type = "instance"
 
-   tags = var.tags
-}
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(
+      { Name = each.key },
+      local.common_tags
+    )
+  }
+
+  tags = merge(
+    { Name = each.key },
+    local.common_tags
+  )
 }
