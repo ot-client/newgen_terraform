99a293330bba7e3af1b745c0fa5fc042a3dbe0e5
deepakkushwahax <deepakkushwaha7523@gmail.com>
2026-04-20 16:49:05 +0530

    added

--- modules/terraform-aws-elasticache-redis/output.tf
+++ modules/terraform-aws-elasticache-redis/output.tf
@@ -1,17 +1,17 @@
 output "redis_replication_group_id" {
-  value       = aws_elasticache_replication_group.this.id
+  value       = aws_elasticache_replication_group.cluster.id
   description = "Redis replication group ID"
 }
 output "redis_primary_endpoint" {
-  value       = aws_elasticache_replication_group.this.primary_endpoint_address
+  value       = aws_elasticache_replication_group.cluster.primary_endpoint_address
   description = "Primary endpoint (write)"
 }
 output "redis_reader_endpoint" {
-  value       = aws_elasticache_replication_group.this.reader_endpoint_address
+  value       = aws_elasticache_replication_group.cluster.reader_endpoint_address
   description = "Reader endpoint (read)"
 }
 output "security_group_id" {
-  value       = var.create_default_security_group ? aws_security_group.this[0].id : null
+  value       = var.create_default_security_group ? aws_security_group.cache_sg[0].id : null
   description = "SG created by module (null if using existing)"
 }
 output "subnet_group_name" {
