6892b1f7beff8111d6664b275676a627a669328d
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-06-26 19:18:20 +0530

    Delete terraform-azure-DNS-Resolver/data.tf

--- terraform-azure-DNS-Resolver/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "network" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "Buildpiper-test"
-    storage_account_name = "terraformstateng"
-    container_name       = "state-file"
-    key                  = "env/dev/network-skeleton/terraform.tfstate"
-  }
-}
