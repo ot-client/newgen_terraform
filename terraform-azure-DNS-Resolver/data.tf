fae51c6230975dcf591e00f52054bf4ba1e01a55
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-08-09 13:42:34 +0530

    Update data.tf

--- terraform-azure-DNS-Resolver/data.tf
+++ terraform-azure-DNS-Resolver/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "network" {
   backend = "azurerm"
   config = {
     resource_group_name  = "Buildpiper-test"
-    storage_account_name = "terraformstateng"
+    storage_account_name = "buildpiperstate"
     container_name       = "state-file"
     key                  = "env/dev/network-skeleton/terraform.tfstate"
   }
