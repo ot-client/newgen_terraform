3322b80a4a6aee3346e9024cb37fa5059926123a
Mohit-opstree <mohit.kumar@opstree.com>
2026-08-10 14:07:17 +0530

    change

--- modules/terraform-azure-storage-account/output.tf
+++ modules/terraform-azure-storage-account/output.tf
@@ -3,10 +3,16 @@ output "storage_account_id" {
   value       = azurerm_storage_account.storage_account.id
 }
 
+output "storage_account_name" {
+  description = "The name of the storage account."
+  value       = azurerm_storage_account.storage_account.name
+}
+
 output "primary_blob_endpoint" {
   description = "The primary blob endpoint."
   value       = azurerm_storage_account.storage_account.primary_blob_endpoint
 }
+
 
 output "file_share_ids" {
   description = "Map of file share names to their resource IDs."
